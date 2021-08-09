a5494012902a1a16f99eeb26c26bca34734cc8e0
Zulkar <zulkar0981@gmail.com>
2021-08-09 13:07:34 -0400

    Initial check-in code.

---
+++ Git/src/practices/practice1.java
@@ -0,0 +1,9 @@
+package practices;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+}
---
+++ Git/src/practices/practice2.java
@@ -0,0 +1,8 @@
+package practices;
+
+public class practice2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hey");
+	}
+}
